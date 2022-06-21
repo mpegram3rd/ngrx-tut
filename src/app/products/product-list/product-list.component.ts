50a4102021748f992fa5f512c949145e25fc13d5
Macon Pegram <mpegram@captechconsulting.com>
2022-06-21 10:18:56 -0400

    First hooks for NgRx

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'pm-product-list',
@@ -22,7 +23,8 @@ export class ProductListComponent implements OnInit, OnDestroy {
   selectedProduct: Product | null;
   sub: Subscription;
 
-  constructor(private productService: ProductService) { }
+  constructor(private store: Store<any>,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
     this.sub = this.productService.selectedProductChanges$.subscribe(
@@ -33,6 +35,15 @@ export class ProductListComponent implements OnInit, OnDestroy {
       next: (products: Product[]) => this.products = products,
       error: err => this.errorMessage = err
     });
+
+    // TODO Unsubscribe later
+    this.store.select('products').subscribe(
+        products => {
+          // when app first start state may not yet be defined
+          if (products) {
+            this.displayCode = products.showProductCode;
+          }
+    });
   }
 
   ngOnDestroy(): void {
@@ -40,7 +51,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
   }
 
   checkChanged(): void {
-    this.displayCode = !this.displayCode;
+    this.store.dispatch({ type: '[Product] Toggle Product Code' });
   }
 
   newProduct(): void {
