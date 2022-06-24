56af6abc8e45ffda1082cdcbc4457fea89a3a15c
Macon Pegram <mpegram@captechconsulting.com>
2022-06-23 21:26:00 -0400

    Enabled OnPush Change Detection

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Product } from '../product';
 
@@ -6,7 +6,8 @@ import { Product } from '../product';
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductListComponent  {
   pageTitle = 'Products';
